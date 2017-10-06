78a31149570c3ac8d2cbaed00bccea6efb898b9c
choihouk <choihoug@gmail.com>
2017-10-06 20:15:21 +0900

    영현파일 추가

---
+++ app/src/main/java/com/example/acer/login/Profile_Tab/Write_Related/Stamp_Fragment.java
@@ -0,0 +1,133 @@
+package com.example.acer.login.Profile_Tab.Write_Related;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.acer.login.Login_Related.SharedPrefManager;
+import com.example.acer.login.Profile_Tab.Stamp_Related.Stamp_Fragment_sub;
+import com.example.acer.login.R;
+
+
+public class Stamp_Fragment extends Fragment {
+    String userLevel, userExp;
+    TextView showLevel;
+    String set;
+
+    ProgressBar progressBar;
+    Handler handler;
+
+    int maxExp = 0;
+    int myExp = 0;
+    int UL;
+
+    ImageView wheel;
+    ImageView levelname;
+    ImageView levelbar;
+    ImageButton stampcollect;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_stamp, container, false);
+
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+        showLevel = (TextView) rootView.findViewById(R.id.showLevel);
+        wheel = (ImageView) rootView.findViewById(R.id.wheel);
+        levelname = (ImageView) rootView.findViewById(R.id.levelname);
+        levelbar = (ImageView) rootView.findViewById(R.id.levelbar);
+        SharedPrefManager sharedPrefManager = SharedPrefManager.getInstance(rootView.getContext());
+        userLevel = sharedPrefManager.getUserLevel();
+        userExp = sharedPrefManager.getUserEXP();
+        set = "현재레벨은  " + userLevel +"이며, " + "다음레벨까지 " + (maxExp-myExp) +"남았습니다.";
+        showLevel.setText(set);
+
+        stampcollect = (ImageButton) rootView.findViewById(R.id.stampcollect);
+
+        stampcollect.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Create new fragment and transaction
+                Fragment anyFragment = new Stamp_Fragment_sub();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.container, anyFragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+
+            }
+        });
+
+
+
+
+
+//레벨에 따른 이미지 변경
+        handler = new Handler() {
+            public void handleMessage(Message message) {
+                maxExp = 100 + 30 * (Integer.parseInt(userLevel) - 1) * (Integer.parseInt(userLevel) + 6);
+                myExp = Integer.parseInt(userExp);
+
+                progressBar.setMax(maxExp);
+                progressBar.setProgress(myExp);
+
+                handler.sendEmptyMessageDelayed(10, 1000);
+            }
+        };
+        handler.sendEmptyMessageDelayed(10, 0);
+
+        UL = Integer.parseInt(userLevel);
+
+
+        switch (UL){
+
+            case 1:
+                wheel.setImageResource(R.drawable.woodwheel);
+                levelname.setImageResource(R.drawable.woodname);
+                levelbar.setImageResource(R.drawable.woodbar);
+                break;
+            case 2:
+                wheel.setImageResource(R.drawable.stonewheel);
+                levelname.setImageResource(R.drawable.stonename);
+                levelbar.setImageResource(R.drawable.stonebar);
+                break;
+            case 3:
+                wheel.setImageResource(R.drawable.tirewheel);
+                levelname.setImageResource(R.drawable.tirename);
+                levelbar.setImageResource(R.drawable.tirebar);
+                break;
+            case 4:
+                wheel.setImageResource(R.drawable.silverwheel);
+                levelname.setImageResource(R.drawable.silvername);
+                levelbar.setImageResource(R.drawable.silverbar);
+                break;
+            case 5:
+                wheel.setImageResource(R.drawable.goldwheel);
+                levelname.setImageResource(R.drawable.goldname);
+                levelbar.setImageResource(R.drawable.goldbar);
+                break;
+            case 6:
+                wheel.setImageResource(R.drawable.diamondwheel);
+                levelname.setImageResource(R.drawable.diamondname);
+                levelbar.setImageResource(R.drawable.diamondbar);
+                break;
+
+            }
+            return rootView;
+        }
+
+    }
+
+
