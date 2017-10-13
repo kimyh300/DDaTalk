def746b2ef40c208978e70203383674c1e2d85c6
ghdrms1992 <ghdrms1992@gmail.com>
2017-10-13 20:11:57 +0900

    image from album v1

--- app/src/main/java/com/example/acer/login/Profile_Tab/MyPage_Related/MyPage_SubActivity.java
+++ app/src/main/java/com/example/acer/login/Profile_Tab/MyPage_Related/MyPage_SubActivity.java
@@ -1,7 +1,9 @@
 package com.example.acer.login.Profile_Tab.MyPage_Related;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,6 +12,8 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -34,6 +38,10 @@ public class MyPage_SubActivity extends AppCompatActivity {
     private static final int PICK_FROM_CAMERA = 0;
     private static final int PICK_FROM_ALBUM = 1;
     //private static final int PICK_FROM_IMAGE = 2;
+
+    private static final int REQUEST_CAMERA = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 2;
+
 
     /*private Uri mImageCaptureUri;
     private ImageView iv_UserPhoto;
@@ -68,12 +76,6 @@ public class MyPage_SubActivity extends AppCompatActivity {
         ImageButton logoutButton = (ImageButton) findViewById(R.id.logoutButton);
         ImageButton photoButton = (ImageButton) findViewById(R.id.photoButton);
 
-        /*subExitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });*/
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,16 +90,36 @@ public class MyPage_SubActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MyPage_SubActivity.this, "회원탈퇴에 성공했습니다.", Toast.LENGTH_LONG).show();
-                MyPage_SubActivity.this.finish();
+                SharedPrefManager.getInstance(getApplicationContext()).logout();
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
 
         photoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int permissionReadStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+                int permissionWriteStorage = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if(permissionReadStorage == PackageManager.PERMISSION_DENIED || permissionWriteStorage == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(MyPage_SubActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_EXTERNAL_STORAGE);
+                } else {
+                    Toast.makeText(getApplicationContext(), "camera permission authorized", Toast.LENGTH_LONG).show();
+                }
+
+
                 DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
+                        int permissionCamera = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA);
+                        if(permissionCamera == PackageManager.PERMISSION_DENIED) {
+                            ActivityCompat.requestPermissions(MyPage_SubActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "camera permission authorized", Toast.LENGTH_LONG).show();
+                        }
+
                         doTakePhotoAction();
                     }
                 };
@@ -105,6 +127,7 @@ public class MyPage_SubActivity extends AppCompatActivity {
                 DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
                         doTakeAlbumAction();
                     }
                 };
@@ -126,9 +149,40 @@ public class MyPage_SubActivity extends AppCompatActivity {
         });
 
 
-
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.CAMERA)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            //resultText.setText("camera permission authorized");
+                        } else {
+                            Toast.makeText(getApplicationContext(), "camera permission denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+                break;
+            case REQUEST_EXTERNAL_STORAGE:
+                for (int i = 0; i < permissions.length; i++) {
+                    String permission = permissions[i];
+                    int grantResult = grantResults[i];
+                    if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        if(grantResult == PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(getApplicationContext(), "read/write storage permission authorized", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "read/write storage permission denied", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+                break;
+        }
+    }
 
 
 
@@ -167,7 +221,8 @@ public class MyPage_SubActivity extends AppCompatActivity {
             case PICK_FROM_ALBUM: {
                 /*mImageCaptureUri = data.getData();
                 Log.d("SmartWheel", mImageCaptureUri.getPath().toString());*/
-                SendPicture(data);
+                try{SendPicture(data);}
+                catch (Exception e) { Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();}
                 break;
             }
             case PICK_FROM_CAMERA: {
@@ -198,14 +253,21 @@ public class MyPage_SubActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        assert exif != null;
-        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        int exifDegree = exifOrientationToDegrees(exifOrientation);
 
-        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
-        try{imageView.setImageBitmap(rotate(bitmap, exifDegree));}//이미지 뷰에 비트맵 넣기
-        catch(Exception e){Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();}
+        if (exif != null) {
+            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            int exifDegree = exifOrientationToDegrees(exifOrientation);
 
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
+            try {
+                imageView.setImageBitmap(rotate(bitmap, exifDegree));
+            }//이미지 뷰에 비트맵 넣기
+            catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        Toast.makeText(getApplicationContext(), exif.toString(), Toast.LENGTH_LONG).show();
 
     }
 
@@ -234,13 +296,12 @@ public class MyPage_SubActivity extends AppCompatActivity {
 
     public String getRealPathFromURI(Uri contentUri) {
         String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);  //managedQuery(contentUri, proj, null, null, null);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
         cursor.moveToFirst();
         return cursor.getString(column_index);
 
     }
-
 
 }
 
