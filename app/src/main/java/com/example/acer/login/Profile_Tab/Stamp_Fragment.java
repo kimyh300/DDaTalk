14eb11360027cbdfd37998e5f8af9182aa405885
kimyh300 <kimyh910508@gmail.com>
2017-10-17 11:31:09 +0900

    volley send and bring

--- app/src/main/java/com/example/acer/login/Profile_Tab/Stamp_Fragment.java
+++ app/src/main/java/com/example/acer/login/Profile_Tab/Stamp_Fragment.java
@@ -1,12 +1,12 @@
 package com.example.acer.login.Profile_Tab;
 
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -17,29 +17,40 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.acer.login.Login_Related.SharedPrefManager;
 import com.example.acer.login.Profile_Tab.Stamp_Related.Stamp_Fragment_sub;
 import com.example.acer.login.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Stamp_Fragment extends Fragment {
-    String userLevel, userExp;
     TextView showLevel;
-    String set;
-
     ProgressBar progressBar;
-    Handler handler;
-
-
-    int maxExp = 0;
-    int myExp = 0;
-    int UL;
+    RequestQueue requestQueue;
 
     ImageView wheel;
     ImageView levelname;
     ImageView levelbar;
     ImageButton stampcollect;
+
+    String L,E;
+    int maxExp;
+
 
     public void setProgressBarColor(ProgressBar progressBar, int newColor){
         LayerDrawable ld = (LayerDrawable) progressBar.getProgressDrawable();
@@ -49,7 +60,7 @@ public class Stamp_Fragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_stamp, container, false);
 
         progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
@@ -57,14 +68,7 @@ public class Stamp_Fragment extends Fragment {
         wheel = (ImageView) rootView.findViewById(R.id.wheel);
         levelname = (ImageView) rootView.findViewById(R.id.levelname);
         levelbar = (ImageView) rootView.findViewById(R.id.levelbar);
-        SharedPrefManager sharedPrefManager = SharedPrefManager.getInstance(rootView.getContext());
-        userLevel = sharedPrefManager.getUserLevel();
-        userExp = sharedPrefManager.getUserEXP();
-        set = "현재레벨은  " + userLevel +"이며, " + "다음레벨까지 " + (maxExp-myExp) +"남았습니다.";
-        showLevel.setText(set);
-
-
-
+        final SharedPrefManager sharedPrefManager = SharedPrefManager.getInstance(rootView.getContext());
 
 
 // 서브스탬프로 들어가기
@@ -83,97 +87,181 @@ public class Stamp_Fragment extends Fragment {
             }
         });
 
-//레벨에 따른 이미지 변경
-        maxExp = 100 + 30 * (Integer.parseInt(userLevel) - 1) * (Integer.parseInt(userLevel) + 6);
-        myExp = Integer.parseInt(userExp);
+        requestQueue = Volley.newRequestQueue(getActivity().getApplication());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://104.198.211.126/getExp.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.equals("0 results")){
+                    Toast.makeText(getActivity().getApplication(),"데이터 가져오기 성공",Toast.LENGTH_LONG).show();
 
-        progressBar.setMax(maxExp);
-        progressBar.setProgress(myExp);
 
-        if(myExp >= maxExp) {
-            UL++;
-            }
+                    try {
+                        JSONArray arr = new JSONArray(response);
+                        for(int i =0; i<arr.length(); i++){
+                            JSONObject obj =  arr.getJSONObject(i);
+                            L=obj.getString("level");
+                            E=obj.getString("exp");
+                            int Ee = Integer.parseInt(E);
+                            int Ll = Integer.parseInt(L);
+                            maxExp = 100 + 30 * (Integer.parseInt(L)- 1) * (Integer.parseInt(L) + 6);
+                            String showExp = String.valueOf(maxExp - Integer.parseInt(E));
 
-        UL = Integer.parseInt(userLevel);
 
-        switch (UL){
 
-            case 1:
-                wheel.setImageResource(R.drawable.woodwheel);
-                levelname.setImageResource(R.drawable.woodname);
-                levelbar.setImageResource(R.drawable.woodbar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#edb043"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#af740b"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#7a520a"));
-            }
-                break;
-            case 2:
-                wheel.setImageResource(R.drawable.stonewheel);
-                levelname.setImageResource(R.drawable.stonename);
-                levelbar.setImageResource(R.drawable.stonebar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#e5e5e5"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#bababa"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#7c7c7c"));
-            }
-                break;
-            case 3:
-                wheel.setImageResource(R.drawable.tirewheel);
-                levelname.setImageResource(R.drawable.tirename);
-                levelbar.setImageResource(R.drawable.tirebar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#718aa8"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#47557f"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#1f2a49"));
-            }
-                break;
-            case 4:
-                wheel.setImageResource(R.drawable.silverwheel);
-                levelname.setImageResource(R.drawable.silvername);
-                levelbar.setImageResource(R.drawable.silverbar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#e5e5e5"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#99999"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#666666"));
-            }
-                break;
-            case 5:
-                wheel.setImageResource(R.drawable.goldwheel);
-                levelname.setImageResource(R.drawable.goldname);
-                levelbar.setImageResource(R.drawable.goldbar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#ffce00"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#f99f00"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#f97600"));
-            }
-                break;
-            case 6:
-                wheel.setImageResource(R.drawable.diamondwheel);
-                levelname.setImageResource(R.drawable.diamondname);
-                levelbar.setImageResource(R.drawable.diamondbar);
-            if(myExp > maxExp * 0.0 && myExp <= maxExp * 0.33) {
-                setProgressBarColor(progressBar, Color.parseColor("#e6f0fc"));
-            } else if(myExp > maxExp*0.33 && myExp <= maxExp * 0.66) {
-                setProgressBarColor(progressBar, Color.parseColor("#b4cbf2"));
-            } else if(myExp > maxExp*0.66 && myExp <= maxExp * 1.0) {
-                setProgressBarColor(progressBar, Color.parseColor("#8da8e2"));
-            }
-                break;
 
-        }
+
+                            progressBar.setMax(maxExp);
+                            progressBar.setProgress(Ee);
+
+                            if (Ee >= maxExp ){
+                                Ll++;
+                                updateLevelUp(sharedPrefManager,getActivity().getApplication());
+                            }
+
+
+
+
+                            switch (Ll){
+
+                                case 1:
+                                    wheel.setImageResource(R.drawable.woodwheel);
+                                    levelname.setImageResource(R.drawable.woodname);
+                                    levelbar.setImageResource(R.drawable.woodbar);
+                                    if(Ee > maxExp * 0.0 && Ee <= maxExp * 0.33) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#edb043"));
+                                    } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#af740b"));
+                                    } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#7a520a"));
+                                    }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+
+                                    break;
+                                case 2:
+                                    wheel.setImageResource(R.drawable.stonewheel);
+                                    levelname.setImageResource(R.drawable.stonename);
+                                    levelbar.setImageResource(R.drawable.stonebar);
+                                    if(Ee > maxExp * 0.0 && Ee <=maxExp * 0.33) {
+                                    setProgressBarColor(progressBar, Color.parseColor("#e5e5e5"));
+                                } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                    setProgressBarColor(progressBar, Color.parseColor("#bababa"));
+                                } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                    setProgressBarColor(progressBar, Color.parseColor("#7c7c7c"));
+                                }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+                                break;
+                                case 3:
+                                    wheel.setImageResource(R.drawable.tirewheel);
+                                    levelname.setImageResource(R.drawable.tirename);
+                                    levelbar.setImageResource(R.drawable.tirebar);
+                                    if(Ee > maxExp * 0.0 && Ee <= maxExp * 0.33) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#718aa8"));
+                                    } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#47557f"));
+                                    } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#1f2a49"));
+                                    }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+                                    break;
+                                case 4:
+                                    wheel.setImageResource(R.drawable.silverwheel);
+                                    levelname.setImageResource(R.drawable.silvername);
+                                    levelbar.setImageResource(R.drawable.silverbar);
+                                    if(Ee > maxExp * 0.0 && Ee <= maxExp * 0.33) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#e5e5e5"));
+                                    } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#99999"));
+                                    } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#666666"));
+                                    }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+                                    break;
+                                case 5:
+                                    wheel.setImageResource(R.drawable.goldwheel);
+                                    levelname.setImageResource(R.drawable.goldname);
+                                    levelbar.setImageResource(R.drawable.goldbar);
+                                    if(Ee > maxExp * 0.0 && Ee <= maxExp * 0.33) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#ffce00"));
+                                    } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#f99f00"));
+                                    } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#f97600"));
+                                    }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+                                    break;
+                                case 6:
+                                    wheel.setImageResource(R.drawable.diamondwheel);
+                                    levelname.setImageResource(R.drawable.diamondname);
+                                    levelbar.setImageResource(R.drawable.diamondbar);
+                                    if(Ee > maxExp * 0.0 && Ee <= maxExp * 0.33) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#e6f0fc"));
+                                    } else if(Ee > maxExp*0.33 && Ee <= maxExp * 0.66) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#b4cbf2"));
+                                    } else if(Ee > maxExp*0.66 && Ee <= maxExp * 1.0) {
+                                        setProgressBarColor(progressBar, Color.parseColor("#8da8e2"));
+                                    }
+                                    showLevel.setText("현재레벨은  " + L +"이며, " + "다음레벨까지 " + (showExp) +"남았습니다.");
+                                    break;
+
+                            }
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity().getApplication(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", sharedPrefManager.getUserEmail());
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+
         return rootView;
     }
 
-}
 
+    private void updateLevelUp(final SharedPrefManager sharedPrefManager, final Context context){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://104.198.211.126/updateLevelUp.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(response);
+                    if(!obj.getBoolean("error")){
+                        Toast.makeText(context,obj.getString("message"),Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context,error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", sharedPrefManager.getUserEmail());
+
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+    }
+
+}
